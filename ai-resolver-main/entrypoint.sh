7f2a24ef99ef8a669ca480d48ad940e35ef9ad21
kris <lamia.mortis@icloud.com>
2023-07-28 01:36:55 +0200

    [00000-1] Implementing uninterrupting flow

--- ai-resolver-main/entrypoint.sh
+++ ai-resolver-main/entrypoint.sh
@@ -8,13 +8,13 @@ test $VERSION_BRANCH
 
 WWW_ROOT="/var/www/ai-resolver-main"
 
-if [ ! -d $WWW_ROOT]; then
+if [ ! -d $WWW_ROOT ]; then
     FIRST_RUN=true
     mkdir -p $WWW_ROOT
 
     pushd $WWW_ROOT
 
-        git clone -b "$VERSION_BRANCH" https://github.com/lamia-mortis/ai-resolver-main.git . 
+        git clone -b "$VERSION_BRANCH" https://github.com/lamia-mortis/ai-resolver-main.git .
         sleep 30
     popd
 fi 
@@ -22,27 +22,24 @@ fi
 chown -R 777 $WWW_ROOT
 cp $WWW_ROOT/.env.example $WWW_ROOT/.env
 
-pushd 
-    php${PHP_VERSION} /usr/bin/composer install
-    php${PHP_VERSION} /usr/bin/composer dump-autoload -o
+pushd $WWW_ROOT
+    php${PHP_VERSION} /usr/local/bin/composer install
+    npm install
 popd
-
-npm ci
 
 touch $WWW_ROOT/storage/logs/laravel.log
 
-if $FIRST_RUN; 
-then 
+if $FIRST_RUN; then 
     pushd $WWW_ROOT
         php${PHP_VERSION} artisan migrate
         php${PHP_VERSION} artisan db:seed
     popd
-else 
-    pushd $WWW_ROOT
-        php${PHP_VERSION} artisan db:fresh --seed
-    popd
 fi
 
 pushd $WWW_ROOT
-    php${PHP_VERSION} artisan serve
+    # by default containers start in bridge network, 
+    # inside which the host available by the address 0.0.0.0
+    php${PHP_VERSION} artisan key:generate
+    npm run build
+    php${PHP_VERSION} artisan serve --host 0.0.0.0
 popd
--- ai-resolver-mind-ms/entrypoint.sh
+++ ai-resolver-mind-ms/entrypoint.sh
@@ -5,22 +5,22 @@ set -x
 test $VERSION_BRANCH
 
 WWW_ROOT="/var/www/ai-resolver-mind-ms"
+export APP_HOST="0.0.0.0"
 
-if [ ! -d $WWW_ROOT]; then
-    FIRST_RUN=true
-    mkdir -p $WWW_ROOT
+if [ ! -d $WWW_ROOT ]; then
+  FIRST_RUN=true
+  mkdir -p $WWW_ROOT
 
-    pushd $WWW_ROOT
+  pushd $WWW_ROOT
 
-        git clone -b "$VERSION_BRANCH" https://github.com/lamia-mortis/ai-resolver-mind-ms.git . 
-        sleep 30
-    popd
+    git clone -b "$VERSION_BRANCH" https://github.com/lamia-mortis/ai-resolver-mind-ms.git .
+    sleep 30
+  popd
 fi 
 
 chown -R 777 $WWW_ROOT
 
-export GOPATH=$WWW_ROOT
-
 pushd $WWW_ROOT
-    go mod download
+  go mod download && go mod verify
+  go run main.go
 popd
